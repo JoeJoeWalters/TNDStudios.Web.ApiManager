98c71f93f974bbc01497fd2b1adc0fc067a39221
TNDStudios <info@thenakeddeveloper.com>
2019-02-26 13:25:31 +0000

    Removed unneeded reference to .net framework mvc

--- TNDStudios.Web.ApiManager/TNDStudios.Web.ApiManager/Security/Authentication/UserAuthenticator.cs
+++ TNDStudios.Web.ApiManager/TNDStudios.Web.ApiManager/Security/Authentication/UserAuthenticator.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http.Headers;
 using System.Text;
-using System.Threading.Tasks;
 using TNDStudios.Web.ApiManager.Security.Objects;
 
 namespace TNDStudios.Web.ApiManager.Security.Authentication
