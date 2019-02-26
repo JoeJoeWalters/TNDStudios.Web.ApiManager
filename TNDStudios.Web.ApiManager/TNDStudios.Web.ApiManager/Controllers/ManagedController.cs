53202681818efd3db2709d251cd117503ab6b863
TNDStudios <info@thenakeddeveloper.com>
2019-02-26 15:38:58 +0000

    Added example base controller for managed controllers

---
+++ TNDStudios.Web.ApiManager/TNDStudios.Web.ApiManager/Controllers/ManagedController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TNDStudios.Web.ApiManager.Security.Objects;
+
+namespace TNDStudios.Web.ApiManager.Controllers
+{
+    /// <summary>
+    /// Managed controller to handle passing context of the user etc. to the 
+    /// controllers that inherit it 
+    /// </summary>
+    public class ManagedController : ControllerBase
+    {
+        public SecurityUser CurrentUser
+        {
+            get => new SecurityUser()
+            {
+                Claims = new List<SecurityClaim>()
+                {
+                    new SecurityClaim()
+                    {
+                        Companies = new List<String> { "ba" },
+                        Name = "admin",
+                        Permissions = new List<String>(){ "read", "write", "delete" }
+                    }
+                }
+            };
+        }
+    }
+}
